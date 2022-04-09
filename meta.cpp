b9054c32c2f3a8b7c94a7c8bed73e5d95f29c138
SkippieDippie <skippiedipie@gmail.com>
2022-04-09 15:30:17 +0200

    Initial commit. Part 1.

---
+++ meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2782616297;
+name = "Stalker Stuff Gear";
+timestamp = 5249535564538466117;
---
+++ mod.cpp
@@ -0,0 +1,10 @@
+name = "Stalker stuff gear"; // Name of your mod
+author = "The man who bought the world"; // Affects Arma 3 Launcher, when the mod are loaded as local
+logo = "logoGear.paa"; // Logo displayed in the main menu
+logoOver = "logoGear.paa"; // When the mouse is over, in the main menu
+tooltip = "Get out of here stalker!";
+overview = "Pretty cool stalker mod, needs fixing doe."; // Supports structured text
+hideName = 0; // Hide the extension name
+hidePicture	= 0;// Hide the extension menu
+logoSmall = "logoGearSmall.paa"; // Display in creative lists, next to the entities added by the mod
+
